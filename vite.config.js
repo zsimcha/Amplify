003963476fef0e4e085381a064234421356edd82
Zachary Shron <zsimcha1@gmail.com>
2026-03-06 09:30:55 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     historyApiFallback: true,
+  },
+  build: {
+    // Suppress the warning limit
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        // Split out large dependencies into their own optimized chunks
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('lucide-react')) {
+              return 'vendor_icons';
+            }
+            if (id.includes('@supabase')) {
+              return 'vendor_supabase';
+            }
+            return 'vendor'; // all other node_modules
+          }
+        }
+      }
+    }
   }
 })
