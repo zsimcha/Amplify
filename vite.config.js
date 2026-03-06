4679ff283c39381968f8b0bf5d9c071337935614
Zachary Shron <zsimcha1@gmail.com>
2026-03-06 09:16:29 -0500

    404 fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This tells Vite to always return index.html for unknown URLs,
+    // allowing react-router to handle the page changes
+    historyApiFallback: true, 
+  }
 })
